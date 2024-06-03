a06ca727d3387e281da643786d8e460b5eec53f9
Arda1775 <145455866+Arda1775@users.noreply.github.com>
2024-06-03 20:31:16 +0300

    Add files via upload

---
+++ MailDogrulama/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MailDogrulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Kullanici_adi;
+            Kullanici_adi = textBox1.Text;
+            if (Kullanici_adi == "milliegitim@meb.k12.tr")
+            {
+                MessageBox.Show("Kullanıcı sisteme kayıtlıdır.");
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı Adınız yanlıştır");
+            }
+        }
+    }
+}
---
+++ ortalamageçtikaldı/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ortalamageçtikaldı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Byte ortalama, devamsizlik;
+            ortalama = Convert.ToByte(textBox1.Text);
+            devamsizlik = Convert.ToByte(textBox2.Text);
+            if (ortalama > 49 && devamsizlik < 30)
+            {
+                label4.Text = "geçtiniz!";
+            }
+            else
+            {
+                label4.Text= "kaldınız! ";   
+            }
+        }
+    }
+}
