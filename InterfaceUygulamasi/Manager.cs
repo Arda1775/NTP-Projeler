12753c4c548779347cafca7d055425be85cab6b3
Arda1775 <145455866+Arda1775@users.noreply.github.com>
2024-06-03 20:30:21 +0300

    Add files via upload

---
+++ InterfaceUygulamasi/Manager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceUygulamasi
+{
+    class Manager : IWorker,ISalary,IEat
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Manager Yemek Yer");
+        }
+
+        public void GetSlary()
+        {
+            Console.WriteLine("Yönetici Maaş Alır");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Manager Çalışıyor");
+        }
+    }
+}
---
+++ InterfaceUygulamasi/Worker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceUygulamasi
+{
+    class Worker : IWorker,ISalary,IEat
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Worker Yemek Yer");
+        }
+
+        public void GetSlary()
+        {
+            Console.WriteLine("Workerk Maaş Alır");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Worker Çalışıyor");
+        }
+    }
+}
