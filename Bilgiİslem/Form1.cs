be597f0062e1fc0e54df25852a68a26f89250406
Arda1775 <145455866+Arda1775@users.noreply.github.com>
2024-06-03 20:22:51 +0300

    Add files via upload

---
+++ Bilgiİslem/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bilgiİslem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int not;
+            not = Convert.ToInt16(textBox1.Text);
+            if (not >= 85 && not <= 100)
+            {
+                MessageBox.Show("Takdir Belgesi Almaya Hak Kazandınız.");
+            }
+            else if (not >= 70 && not < 85) 
+            {
+                MessageBox.Show("Teşekkür Belgesi Almaya Hak Kazandınız.");
+            }
+            
+             else if (not >= 50 && not < 70) 
+            {
+                MessageBox.Show("Belgesiz Sınıf Geçtiniz");
+            }
+            else if (not >= 0 && not < 50) 
+            {
+                MessageBox.Show("Sınıfta Kaldınız");
+            }
+            else if (not>100)
+            {
+                MessageBox.Show("Geçersiz Sayı Girdiniz");
+            }
+            else if (not<0)
+            {
+                MessageBox.Show("Geçersiz Sayı Girdiniz");
+            }
+        }
+    }
+}
