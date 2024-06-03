1f092b744372cc8acabad724a15a29843f3bb34f
Arda1775 <145455866+Arda1775@users.noreply.github.com>
2024-06-03 20:34:41 +0300

    Add files via upload

---
+++ sifreGüç/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sifreGüç
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayi1, sayi2, sonuc;
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            sayi2 = Convert.ToDouble(textBox2.Text);
+
+            if (radioButton1.Checked)
+            {
+                sonuc = sayi1 + sayi2;
+            }
+            else if (radioButton2.Checked)
+            {
+                sonuc = sayi1 - sayi2;
+            }
+            else if (radioButton3.Checked)
+            {
+                sonuc = sayi1 * sayi2;
+            }
+            else
+            {
+                sonuc = sayi1 / sayi2;
+            }
+
+            textBox3.Text = sonuc.ToString();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            int karakter = textBox5.Text.Length;
+            if (karakter > 0 && karakter < 4)
+            {
+                progressBar1.Value = 1;
+                progressBar1.ForeColor = Color.Red;
+                label6.Text = "Zayıf";
+            }
+            else if (karakter >= 4 && karakter < 8)
+            {
+                progressBar1.Value = 2;
+                progressBar1.ForeColor = Color.Yellow;
+                label6.Text = "Orta";
+            }
+            else if (karakter >=8)
+            {
+                progressBar1.Value = 3;
+                progressBar1.ForeColor = Color.Green;
+                label6.Text = "Güçlü";
+            }
+            else
+            {
+                progressBar1.Value = 0;
+            }
+        }
+    }
+}
---
+++ sınıfgecme/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sınıfgecme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            short sayi1;
+            sayi1 = Convert.ToInt16(textBox1.Text);
+            if (sayi1 >=50 && sayi1 <=100 )
+            {
+                MessageBox.Show("Sınıfı Geçtiniz");
+            }
+            if (sayi1 <50 && sayi1 >=10 )
+                {
+                MessageBox.Show("sınıfı sorunlu geçtiniz");
+            }
+            if (sayi1<10)
+            {
+                MessageBox.Show("Sınıfta kaldınız");
+            }
+            else
+            {
+                MessageBox.Show("geçerli bir sayı giriniz");
+            }
+        }
+    }
+}
