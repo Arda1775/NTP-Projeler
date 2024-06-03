2a207d0ddd85aaf4beacc5ce854027b039bc0f7d
Arda1775 <145455866+Arda1775@users.noreply.github.com>
2024-06-03 20:28:34 +0300

    Add files via upload

---
+++ hesapMakinesi+ProggesBar/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hesapMakinesi_ProggesBar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, sonuc;
+            sayi1 = Convert.ToInt32(textBox1.Text);
+            sayi2 = Convert.ToInt32(textBox2.Text);
+
+            if (radioButton1.Checked)
+            {
+                sonuc = sayi1 + sayi2;
+            }
+            if (radioButton2.Checked)
+            {
+                sonuc = sayi1 - sayi2;
+            }
+            if (radioButton3.Checked)
+            {
+                sonuc = sayi1 * sayi2;
+            }
+            if (radioButton4.Checked)
+            {
+                sonuc = sayi1 / sayi2;
+            }
+            label4.Text = sonuc.ToString();
+        }
+    }
+}
